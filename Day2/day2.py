dc3a22ac0d0f7cd136ebf6e4747a649da3e832ed
illusion <illusion840@gmail.com>
2024-12-08 16:12:45 -0700

    save_incomplete_2

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,31 +1,29 @@
-with open("Day2/day2_example") as input:
+with open("Day2/day2_input") as input:
     input = input.readlines()
 
 count = 0
 safe = []
 unsafe = []
 for report in input:
+    count += 1
     report = report.split()
-    if all(report[i] <= report[i + 1] for i in range(len(report) - 1)):
+    if all(report[i] <= report[i + 1] for i in range(len(report) - 1)) or all(report[i] >= report[i + 1] for i in range(len(report) - 1)):
         #
         for level in range(len(report) - 1):
             cur_level = report[level]
             next_level = report[level + 1]
-            if int(max(cur_level, next_level)) - int(min(cur_level, next_level)) > 3:
+            if int(abs(int(cur_level) - int(next_level))) > 3 or cur_level == next_level:
+                unsafe.append(report)
                 break
         else:
-            count += 1
-            
-    elif all(report[i] >= report[i + 1] for i in range(len(report) - 1)):
-        #
-        for level in range(len(report) - 1):
-            cur_level = report[level]
-            next_level = report[level + 1]
-            if int(max(cur_level, next_level)) - int(min(cur_level, next_level)) > 3:
-                break
-        else:
-            count += 1
-            
+            safe.append(report)
+
     else:
-        continue
-print(count)
+        unsafe.append(report)
+        
+print("unsafe")
+# print(unsafe)
+print(len(unsafe))
+# print(safe)
+print("safe")
+print(len(safe))
