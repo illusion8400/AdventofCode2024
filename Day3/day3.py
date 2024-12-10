a5aa7c5dc0b72c54e2cb87d3e3d8b4c1777cf34b
illusion <illusion840@gmail.com>
2024-12-10 06:53:02 -0700

    day_3

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,45 +1,46 @@
 import re
 
-with open('Day3/day3_example', 'r') as input:
-    input = input.readlines()
+with open('Day3/day3_input', 'r') as input:
+    input = input.read()
 
 pattern = r"mul\(\d+,\d+\)"
 digits_pattern = r"\d+,+\d+"
-dont = r"don't\(\)"
-do = r'do\(\)'
-
-do_or_dont = True
+dont_or_do = r"don't\(\)(.*)do\(\)"
+dont_do_it = r"don't(.*)"
 
 def part1():
     final_multiplications = []
-    for line in input:
-        matches = re.findall(pattern, line)
-        for match in matches:
-            match_tuple = re.findall(digits_pattern, match)
-            for x in match_tuple:
-                x = str(x).split(',')
-                first = x[0]
-                second = x[1]
-                if len(first) > 3 or len(second) > 3:
-                    break
-                final_multiplications.append(int(first) * int(second))
+    matches = re.findall(pattern, input)
+    for match in matches:
+        match_tuple = re.findall(digits_pattern, match)
+        for x in match_tuple:
+            x = str(x).split(',')
+            first = x[0]
+            second = x[1]
+            if len(first) > 3 or len(second) > 3:
+                continue
+            final_multiplications.append(int(first) * int(second))
     return sum(final_multiplications)
 
 def part2():
     final_multiplications = []
-    for line in input:
-        matches = re.findall(pattern, line)
+    remove_dont = re.search(dont_or_do, input).group()
+    # for dont in remove_dont:
+    new_input = input.replace(remove_dont, "")
+    matches = re.findall(pattern, new_input)
+    dont_at_end = re.match(dont_do_it, new_input)
+    if dont_at_end:
+        new_input.replace(dont_at_end, new_input)
+    for match in matches:
+        match_tuple = re.findall(digits_pattern, match)
+        # for x in match_tuple:
+        match_tuple = str(match_tuple[0]).split(',')
+        first = match_tuple[0]
+        second = match_tuple[1]
+        if len(first) > 3 or len(second) > 3:
+            continue
 
-        for match in matches:
-            match_tuple = re.findall(digits_pattern, match)
-            # for x in match_tuple:
-            match_tuple = str(match_tuple[0]).split(',')
-            first = match_tuple[0]
-            second = match_tuple[1]
-            if len(first) > 3 or len(second) > 3:
-                break
-            if do_or_dont:
-                final_multiplications.append(int(first) * int(second))
+        final_multiplications.append(int(first) * int(second))
     return sum(final_multiplications)
 
 print('Part 1: ', part1())
--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # AdventofCode2024
 My solutions for Advent of Code 2024. 2nd year playing. Have my python skills increased? We'll find out.
 8 stars in 2023.
-I work full time so solutions will be late. I'll be working on one's I've missed as time allows me.
+I haven't had the time to complete a lot but I'll keep working on them as I have time.
+I'm using branches until both parts of a day are completed to merge to main.
 
     Day 1: Completed.
+    
+    Day 2: Ugh.. WIP
+
+    Day 3: Part 1 Completed; Part 2 WIP
